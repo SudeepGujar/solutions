72bcf4a81f2ec95c36c010f1f6d58d9ea823dc4d
Sudeep <sudeepsgujar@gmail.com>
2020-07-30 23:37:45 +0530

    Refactoring: Find Closest Elements

--- src/main/java/leetcode/algorithms/FindClosestElements.java
+++ src/main/java/leetcode/algorithms/FindClosestElements.java
@@ -1,21 +1,39 @@
 package leetcode.algorithms;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+/**
+ * Given a sorted array arr, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: arr = [1,2,3,4,5], k = 4, x = 3
+ * Output: [1,2,3,4]
+ * Example 2:
+ *
+ * Input: arr = [1,2,3,4,5], k = 4, x = -1
+ * Output: [1,2,3,4]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= k <= arr.length
+ * 1 <= arr.length <= 10^4
+ * Absolute value of elements in the array and x will not exceed 104
+ */
+
 
 public class FindClosestElements {
 
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         return Arrays.stream(arr)
                 .boxed()
-                .sorted((i1, i2) -> {
-                    final int o1 = i1 - x;
-                    final int o2 = i2 - x;
-                    final int o1Square = o1 * o1;
-                    final int o2Square = o2 * o2;
-                    return o1Square - o2Square;
-                })
+                .sorted(Comparator.comparingInt(i -> Math.abs(i - x)))
                 .limit(k)
                 .sorted()
                 .collect(Collectors.toList());
